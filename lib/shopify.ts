0f2d6e95f4ef6b3d11b636cdeef048a991af7979
varcal-dev <cesarlea2010@hotmail.com>
2026-09-14 23:35:06 -0500

    mod vista productos

--- lib/shopify.ts
+++ lib/shopify.ts
@@ -91,26 +91,29 @@ const PRODUCT_CARD_FIELDS = `
 `
 
 export async function getStorefront() {
-  const query = `
-    query {
-      products(first: 8) {
-        nodes {
-          id
-          title
-          handle
+  const query = `query Storefront {
+    products(first: 8, sortKey: BEST_SELLING) {
+      nodes {
+        ${PRODUCT_CARD_FIELDS}
+      }
+    }
+
+    collections(first: 6) {
+      nodes {
+        id
+        title
+        handle
+        image {
+          url
+          altText
         }
       }
     }
-  `
+  }`
 
   return shopifyFetch<{
-    products: {
-      nodes: {
-        id: string
-        title: string
-        handle: string
-      }[]
-    }
+    products: { nodes: Product[] }
+    collections: { nodes: Collection[] }
   }>(query)
 }
 
